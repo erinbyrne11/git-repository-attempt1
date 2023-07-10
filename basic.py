f27c713e786addde6521dda8727f8515c39841d2
erinbyrne11 <erinbyrnemary1@hotmail.co.uk>
2023-07-10 16:59:46 +0100

    commit one

---
+++ basic.py
@@ -0,0 +1,14 @@
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+
+# Main program
+length = 5
+width = 3
+
+# Calculate the area of the rectangle
+area = calculate_rectangle_area(length, width)
+
+# Print the result
+print("The area of the rectangle is:", area)
